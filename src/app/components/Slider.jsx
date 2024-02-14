d20aef653445124a5f271c381b031d8dc66b40b4
AbolghasemTaziki <a.taziki70@gmail.com>
2024-02-14 19:02:52 +0330

    update slider

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -1,37 +1,37 @@
-"use client"
-import React from 'react';
+"use client";
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import Image from 'next/image';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import Image from "next/image";
 
-
-
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 // import required modules
-import { Pagination, Navigation ,Autoplay} from 'swiper';
+import { Pagination, Navigation, Autoplay } from "swiper";
+import Link from "next/link";
 export default function App() {
   return (
-    <>
+    <div>
       <Swiper
         slidesPerView={1}
         spaceBetween={0}
-        loop= {true}
+        loop={true}
         autoplay={{
           delay: 2500,
-          disableOnInteraction: false,         
+          disableOnInteraction: false,
         }}
         centeredSlides={true}
         navigation={true}
-        modules={[Autoplay,Pagination, Navigation]}
+        modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
-        <div className="w-full ">
+          <div className="w-full ">
             <div className="w-full">
               <Image
                 className="object-cover"
@@ -50,10 +50,14 @@ export default function App() {
                 <span>جوهرها</span>
               </p>
             </div>
+            <div className=" bg-white py-1 px-3 rounded-md cursor-pointer text-black flex items-center gap-4 absolute bottom-1/3 left-1/2 transform -translate-x-1/2 translate-y-16">
+              <Link href="/">تتو ها</Link>
+              <FaLongArrowAltLeft />
+            </div>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-        <div className="w-full ">
+          <div className="w-full ">
             <div className="w-full">
               <Image
                 className="object-cover"
@@ -72,11 +76,16 @@ export default function App() {
                 <span>جوهرها</span>
               </p>
             </div>
+            <div className=" bg-white py-1 px-3 rounded-md cursor-pointer text-black flex items-center gap-4 absolute bottom-1/3 left-1/2 transform -translate-x-1/2 translate-y-16">
+              <Link href="/">تتو ها</Link>
+              <FaLongArrowAltLeft />
+            </div>
           </div>
         </SwiperSlide>
-        
-    
       </Swiper>
-    </>
+      <div className="absolute cursor-pointer bottom-0 left-1/2  transform -translate-x-1/2 -translate-y-1/2 md:block hidden  z-[99]">
+        <Image src="/images/down-page.png" width={20} height={20} alt="down" />
+      </div>
+    </div>
   );
 }
